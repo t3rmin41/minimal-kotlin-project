57f7829cf65daca817432f9d65daf3bb3401203e
t3rmin41 <t33rmin41@gmail.com>
2020-12-26 12:39:25 +0200

    Sample of non-static and static logger

--- src/main/kotlin/com/sample/MainApp.kt
+++ src/main/kotlin/com/sample/MainApp.kt
@@ -4,6 +4,9 @@ import org.slf4j.LoggerFactory
 
 class MainApp {
 
+    private val log = LoggerFactory.getLogger(javaClass) // this logger cannot be referenced in fun main(..) as
+    // non-static variable cannot be referenced in static context
+
     companion object {
         private val logger = LoggerFactory.getLogger(javaClass)
 
